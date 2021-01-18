fd92d4ed591efd5109ae336299e306fd7b332aca
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-01-18 12:34:47 +0100

    created Sponsors page content.

---
+++ src/components/SponsorProfile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import '../styles/SponsorProfile.scss';
+
+const SponsorProfile = ({src, alt, name}) => {
+
+   console.log(name);
+   return(
+      <div className="sponsorProfile">
+         <img src={src} alt={alt} className="sponsorProfile__photo"/>
+         <span className="sponsorProfile__name">{name}</span>
+      </div>
+   );
+};
+
+export default SponsorProfile;
--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
+import SponsorProfile from '../components/SponsorProfile';
+
 import '../styles/Sponsors.scss'
 
 const Sponsors = ({sponsors}) => {
+
+   const sponsorLink = `https://picsum.photos/id/`;
+
+   const sponsorsList = sponsors.map(({author, id}) => (
+      <SponsorProfile src={`${sponsorLink}${id}/300.webp`} alt={`Zdjęcie profilowe sponsora ${author}.`} name={author} key={id}  />
+   ))
+
    return(
-      <div>Sponsorzy</div>
+      <div className="sponsors">
+         <h2 className="sponsors__title"> Serdecznie dziękujemy wszystkim którzy wspierają nasze działania!</h2>
+         <p className="sponsors__italic">Nie liczy się to ile posiadasz, ale ile dajesz innym i jak się z nimi dzielisz. Pomagając innym pomagasz i sobie. Starajcie się zostawić ten świat choć trochę lepszym, niż go zastaliście.</p>
+         <div className="sponsors__profilesWrapper">
+            {sponsorsList}
+         </div>
+         <button className="button">Zostań Sponsorem</button>
+      </div>
    );
 };
 
