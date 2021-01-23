7adf33585edb76fa7a17f349e0c3e9be84a025d9
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-01-23 15:52:27 +0100

    create footer content and add responsiv styles

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
+import { ReactComponent as Facebook } from '../images/facebook-square-brands.svg';
+import { ReactComponent as Instagram } from '../images/instagram-square-brands.svg';
+
 import '../styles/Footer.scss'
+
 
 const Footer = () => {
    return(
       <footer className="footer">
-         Footer
+         <div className="footer__socialmedia">
+            <div className="footer__socialElement">
+               <Instagram className="socialIcon"/>
+               <span className="footer__socialElement__text">@gramydla_gora</span>
+            </div>
+            <div className="footer__socialElement">
+               <Facebook className="socialIcon"/>
+               <span className="footer__socialElement__text">/gramydla_gora</span>
+            </div>
+         </div>
+         <div className="footer__addres">
+            <span className="footer__addres__street">ul. Nieznana 6 Góra, 56-200</span>
+            <span className="footer__addres__phone">tel: +48 669 455 999</span>
+            <span className="footer__addres__email">kontakt@gramydla.pl</span>
+         </div>
+         <div className="footer__copyright">Gramy dla 2020&copy;</div>
       </footer>
    );
 };
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 
 import PhotoCarousel from '../components/PhotoCarousel';
 import MonthSection from "../components/MonthSection";
