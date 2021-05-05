ffded30e603a2c8318cfde5d247c8a08d6c15925
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-05-05 15:38:17 +0200

    gallery patch

--- src/components/GalleryFolder.jsx
+++ src/components/GalleryFolder.jsx
@@ -18,13 +18,13 @@ const GalleryFolder = ({gallery}) => {
    return(
       <div className="folder" onClick={handleClick}>
          <img 
-            src={gallery.okladka.formats.small.url} 
+            src={gallery?.okladka.formats.small?.url} 
             alt="miniatura folderu w galerii" 
             className="folder__image"
          />
-         <span className="folder__date">{gallery.published_at.slice(0, 10)}</span>
-         <span className="folder__title">{gallery.title}</span>
-         <p className="folder__discription">{gallery.opis}</p>
+         <span className="folder__date">{gallery?.published_at.slice(0, 10)}</span>
+         <span className="folder__title">{gallery?.title}</span>
+         <p className="folder__discription">{gallery?.opis}</p>
       </div>
    );
 };
